fa72a3cad1cc531db9764998c1e92558604dbeeb
ldermign <ldermign@student.42.fr>
2021-12-16 15:00:03 +0100

    ajout libft

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,17 +6,19 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 10:34:48 by ldermign          #+#    #+#             */
-/*   Updated: 2021/12/14 21:43:43 by ldermign         ###   ########.fr       */
+/*   Updated: 2021/12/14 21:48:57 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
-# include <unistd.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <sys/time.h>
@@ -24,15 +26,13 @@
 # include <sys/stat.h>
 # include <dirent.h>
 # include <string.h>
-# include <stdio.h>
 # include <errno.h>
-# include <stdlib.h>
 # include <sys/ioctl.h>
 # include <termios.h>
 # include <curses.h>
 # include <term.h>
 # include <fcntl.h>
-# include <colors.h>
+# include "colors.h"
 # include "libft.h"
 # include "ft_printf.h"
 # include "get_next_line.h"
